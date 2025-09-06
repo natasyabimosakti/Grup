5fc93116f0bd815a57fa710838fb0678fd3b01ba
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 13:49:56 +0700

    Update lamab4.js

--- grupb/lamab4.js
+++ grupb/lamab4.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         ORI 2 ERI4
+// @name         ORI LAMA B4
 // @namespace    http://tampermonkey.net/
-// @version      3.02
+// @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri2/eri4.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab4.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab4.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
