8d8c29af342af0abf433d0f1ea66d5a57e9ff562
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 14:03:51 +0700

    Update Barua1.js

--- Thora/Barua1.js
+++ Thora/Barua1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         SHANTI 1
+// @name         Baru A1
 // @namespace    http://tampermonkey.net/
-// @version      3.66
+// @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti1.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti1/shanti1.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua1.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
