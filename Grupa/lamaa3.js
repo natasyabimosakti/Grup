86a252d88d5f39a229cb0962ca3eb337fa0f7314
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 13:31:15 +0700

    Update lamaa3.js

--- Grupa/lamaa3.js
+++ Grupa/lamaa3.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/eri1/eri3.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa3.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa3.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
