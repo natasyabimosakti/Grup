6397a437999da8d424b094734e046b1caa84a0db
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 13:55:11 +0700

    Update lamac1.js

--- Grupc/lamac1.js
+++ Grupc/lamac1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         BO1 PAPA21
+// @name         ORI Lama C1
 // @namespace    http://tampermonkey.net/
-// @version      1.18
+// @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa21.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa2/papa21.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac1.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
