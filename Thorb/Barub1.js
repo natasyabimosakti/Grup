59d524d656efe003b13b9da95eb39aec0181d7da
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 14:10:52 +0700

    Update Barub1.js

--- Thorb/Barub1.js
+++ Thorb/Barub1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         SHANTI 1
+// @name         Baru B1
 // @namespace    http://tampermonkey.net/
-// @version      1.49
+// @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti2/Shanti1.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/Shanti2/Shanti1.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
