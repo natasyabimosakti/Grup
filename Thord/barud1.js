cdf057d01c96e982e9fb8adbab5cfaeff33ea88e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-06 17:06:42 +0700

    Update barud1.js

--- Thord/barud1.js
+++ Thord/barud1.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         ORI PAPA4
+// @name         Baru D1
 // @namespace    http://tampermonkey.net/
-// @version      2.05
+// @version      1.00
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa4.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Eriawan/main/ori/papa1/papa4.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
