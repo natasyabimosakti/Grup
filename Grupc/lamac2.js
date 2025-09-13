e28837ad73f60525c0d38897ef98b802e9d6c36b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-13 18:53:49 +0700

    Update lamac2.js

--- Grupc/lamac2.js
+++ Grupc/lamac2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI Lama CACING 12
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac2.js
@@ -60,8 +60,8 @@ var Comment13 = 'Grup CACING 12';
 var namagroup14 = 'YYYYGTO';
 var Comment14 = 'Grup CACING 12';
 
-var namagroup15 = 'YYYYYRIATOTO';
-var Comment15 = 'Grup CACING 12';
+var namagroup15 = 'TREX';
+var Comment15 = '#HATORIBET*YAYUK5*68*77*87';
 
 var namagroup16 = 'MAYAPADA';
 var Comment16 = 'Grup CACING 12';
