2fc8d1eb2111db393f0c992a764eeb6765c40b75
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-14 12:50:03 +0700

    Update lamaa4.js

--- Grupa/lamaa4.js
+++ Grupa/lamaa4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI Lama ANGIN 14
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa4.js
@@ -64,8 +64,9 @@ var Comment14 =  '#PAPUATOTO=(NUNGGAK200)=27-46';
 var namagroup15 = 'TREX';
 var Comment15 = '#HATORIBET*NENENG93*19*53*00';
 
-var namagroup16 = 'ZZZZMENARA';
-var Comment16 = 'GRUP ANGIN 14';
+var namagroup16 = 'ASIA';
+var Comment16 = '#ASIATOTO = {NEMBE31} = 45*96'; 
+
 
 //Batas
 
