093a9adf9877df9c879682677b93831b1087b9a1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-14 12:59:50 +0700

    Update lamad2.js

--- Grupd/lamad2.js
+++ Grupd/lamad2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA DEMAM 12
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupd/lamad2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupd/lamad2.js
@@ -63,8 +63,8 @@ var Comment14 =  '#PAPUATOTO=(PUNK888)=91-41';
 var namagroup15 = 'TREX';
 var Comment15 = '#HATORIBET*ENAO32*79*74*80';
 
-var namagroup16 = 'MENARAkkkk';
-var Comment16 = 'GRUP DEMAM 12';
+var namagroup16 = 'ASIA';
+var Comment16 = '#ASIATOTO = {ENTOK22} = 62*69'; 
 
 //Batas
 var namagroup17 = 'Jawatengah';
