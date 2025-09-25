dfd38ad259d24e9aa292e31cd3c8495739790f8e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-25 14:59:59 +0700

    Update barud3.js

--- Thord/barud3.js
+++ Thord/barud3.js
@@ -3,8 +3,8 @@
 // @namespace    http://tampermonkey.net/
 // @version      3.4
 // @description  try to take over the world!
-// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
-// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
+// @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud3.js
+// @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud3.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
