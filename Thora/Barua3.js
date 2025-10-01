9922f1afaacbdc31c4a742e320211c134cc6ae3e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-01 17:31:52 +0700

    Update Barua3.js

--- Thora/Barua3.js
+++ Thora/Barua3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru ANGIN 23
 // @namespace    http://tampermonkey.net/
-// @version      3.2
+// @version      3.3
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua3.js
@@ -48,7 +48,7 @@ var namagroup10 = 'XXXKIKOTOTO';
 var Comment10 = '#KIKOTOTO (ICANSEE21) = 11*14';
 
 var namagroup11 = 'LING';
-var Comment11 = 'LING77 (MALIAN19) 09*21';
+var Comment11 = 'LING77 (MALIAN19) 06*21';
 
 var namagroup12 = 'OPUNG';
 var Comment12 = 'OPUNG4D ( ILHAM88 ) : 94*01*77';
