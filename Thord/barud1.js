32a1782c76266b928b9cace17451f42734555787
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-15 21:18:44 +0700

    Update barud1.js

--- Thord/barud1.js
+++ Thord/barud1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru DEMAM 21
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud1.js
@@ -29,8 +29,8 @@ var Comment3 = '(GUDANGTOTO) = (DONGDONG11) 33*35*89';
 var namagroup4 = 'VIP';
 var Comment4 = 'Tok99Toto ( DUPLAK94 ) : 69*79*74';
 
-var namagroup5 = '(WARTO)';
-var Comment5 = '#(WARUNGTOTO) = (ANDARA939) 40*52';
+var namagroup5 = 'BLITAR4D';
+var Comment5 = '#BLITAR4D (ANDARA939) = 40*52';
 
 var namagroup6 = 'K86';
 var Comment6 = 'K86TOTO ( AMPUNN365 ) : 58*29*00';
