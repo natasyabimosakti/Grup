445fd2d794a100ad090389cd39167dcc5f46ba71
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-28 22:35:48 +0700

    Update Barua2.js

--- Thora/Barua2.js
+++ Thora/Barua2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BAru ANGIN 22
 // @namespace    http://tampermonkey.net/
-// @version      3.3
+// @version      3.4
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua2.js
@@ -26,8 +26,8 @@ var Comment1 = '#DIVA4D (JUALAN666) = 43*60*57';
 var namagroup2 = 'TAFSIR';
 var Comment2 = 'SIJITOGEL JERUJI111 99*36*40';
 
-var namagroup3 = 'GTO';
-var Comment3 = '(GUDANGTOTO) = (JAPAN808) 28*16*31';
+var namagroup3 = 'KOITOTO';
+var Comment3 = '#KOITOTO ( JAPAN808 ) 28*16';
 
 var namagroup4 = 'VIP';
 var Comment4 = 'Tok99Toto ( JURANG888 ) : 15*78*10';
