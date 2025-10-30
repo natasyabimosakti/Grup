e4c9cfead0ce96c39d86ea3d4515167b122abf07
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-31 04:32:16 +0700

    Update version and group names in barud4.js

--- Thord/barud4.js
+++ Thord/barud4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru DEMAM 24
 // @namespace    http://tampermonkey.net/
-// @version      3.7
+// @version      3.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thord/barud4.js
@@ -32,7 +32,7 @@ var Comment4 = 'Tok99Toto ( GENDUT81 ) : 52*29*55';
 var namagroup5 = 'BLITAR4D';
 var Comment5 = '#BLITAR4D (DADAKU818) = 55*78';
 
-var namagroup6 = 'K86';
+var namagroup6 = 'K86T';
 var Comment6 = 'K86TOTO ( DIAM001 ) : 96*05*20';
 
 var namagroup7 = 'RIATOTO';
