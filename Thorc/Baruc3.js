8e205a25e73f9349d9a1e9b16c3bc5d94ea7da4d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-31 04:31:02 +0700

    Bump version from 3.5 to 3.6 and update group name

--- Thorc/Baruc3.js
+++ Thorc/Baruc3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru CACING 23
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorc/Baruc3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorc/Baruc3.js
@@ -20,7 +20,7 @@
 var namagroup1 = 'LOGAMTOTO';
 var Comment1 = '#LOGAMTOTO=XXXAGE30= 32*02#';
 
-var namagroup2 = 'K86';
+var namagroup2 = 'K86T';
 var Comment2 = 'K86TOTO ( XBAY875 ) : 16*02*90';
 
 var namagroup3 = 'KOITOTO';
