a63c434f5598d693dd64d2e1ca14baed47e2f221
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-10-31 04:29:33 +0700

    Update version and group names in Barub1.js

--- Thorb/Barub1.js
+++ Thorb/Barub1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru B1
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
@@ -21,7 +21,7 @@
 var namagroup1 = 'VESPA7093L';
 var Comment1 = 'VESPATOGEL (TONGKOL65) 66*08*20';
 
-var namagroup2 = 'K86';
+var namagroup2 = 'K86T';
 var Comment2 = 'K86TOTO ( TUNDER18 ) : 52*38*32';
 
 var namagroup3 = 'KIKOTOTO';
