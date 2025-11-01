6f84451052439a15fe066843d6bf306a5b310657
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-01 20:57:13 +0700

    Update Barub1.js

--- Thorb/Barub1.js
+++ Thorb/Barub1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru B1
 // @namespace    http://tampermonkey.net/
-// @version      3.6
+// @version      3.7
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub1.js
@@ -63,8 +63,8 @@ var Comment14 = '#BLITAR4D (TIKTOK2024) = 94*96';
 var namagroup15 = 'UPINSLOT';
 var Comment15 = '#UPINSLOT ( TONGKOL65 ) 66*08*20 BETTING';
 
-var namagroup16 = 'MELATI188';
-var Comment16 = '*( MELATI188 ) ( TOMATOES288 )  52*38 *BETTING *TOGELONLINE';
+var namagroup16 = 'LIVITOTO';
+var Comment16 = 'TOMATOES288 = 52*38 #LIVITOTO';
 
 var namagroup17 = 'LING';
 var Comment17 = 'LING77 (TASMANIA2) 52*38';
