4bf96cc8c3d9e91d7ed51fb7e12e4c6f8d78dd9d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-01 20:52:56 +0700

    Update Barua1.js

--- Thora/Barua1.js
+++ Thora/Barua1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru ANGIN 21
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.6
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua1.js
@@ -48,8 +48,8 @@ var Comment9 = '#UPINSLOT ( PIQUE020 ) 43*56*86 BETTING';
 var namagroup10 = 'KOITOTO';
 var Comment10 = '#KOITOTO ( PERAK689 ) 67*79';
 
-var namagroup11 = 'MELATI188';
-var Comment11 = '*( MELATI188 ) ( PUCHER333 )  44*25 *BETTING *TOGELONLINE';
+var namagroup11 = 'LIVITOTO';
+var Comment11 = 'PIANG212 = 44*25 #LIVITOTO';
 
 var namagroup12 = 'OPUNG';
 var Comment12 = 'OPUNG4D ( PESAN007 ) : 92*84*79';
@@ -63,7 +63,7 @@ var namagroup14 = 'XXXHITO';
 var Comment14 = '#HITOGEL PUCHER33 67*72*30';
 
 var namagroup15 = 'XXXCITA-4D';
-var Comment15 = 'CITA4D*PONTA670*81*35*09';
+var Comment15 = '*( MELATI188 ) ( PUCHER333 )  44*25 *BETTING *TOGELONLINE';
 
 var namagroup16 = 'BENTOELLLL';
 var Comment16 = 'GRUP ANGIN 21';
