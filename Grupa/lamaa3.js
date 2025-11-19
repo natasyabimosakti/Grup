bfd2aec414feab205a37ff42f72ab5b768186133
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-19 19:06:55 +0700

    Update lamaa3.js

--- Grupa/lamaa3.js
+++ Grupa/lamaa3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI Lama ANING 13
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      4.1
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa3.js
@@ -59,7 +59,7 @@ var Comment13 = '#TOYIBSLOT (MANDRA16) = 26*81';
 var namagroup14 = 'XXXPAPUAT0T0';
 var Comment14 =  '#PAPUATOTO=(MASIKA12)=27-46';
 
-var namagroup15 = 'MARKAS';
+var namagroup15 = 'HAT0R1';
 var Comment15 = '#HATORIBET*MALAY12*30*84*14';
 
 var namagroup16 = 'ASIA';
