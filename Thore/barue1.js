2495ea04797cc9dcd7d7ea1e8d9a611537e5c576
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-12-31 16:12:59 +0700

    Update barue1.js

--- Thore/barue1.js
+++ Thore/barue1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru EMAN 21
 // @namespace    http://tampermonkey.net/
-// @version      3.1
+// @version      3.0
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thore/barue1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thore/barue1.js
