c25cb6d7edc96282cc372f7bb573446ed9a22b97
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-02-15 19:51:20 +0700

    Update Barub4.js

--- Thorb/Barub4.js
+++ Thorb/Barub4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BAru B4
 // @namespace    http://tampermonkey.net/
-// @version      3.9
+// @version      4.0
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub4.js
@@ -62,8 +62,8 @@ var Comment14 = '#BLITAR4D (SENADA17) = 62*09';
 var namagroup15 = 'UPINSLOT';
 var Comment15 = '#UPINSLOT ( SHINY111 ) 36*32*39 BETTING';
 
-var namagroup16 = 'LIVITOTO';
-var Comment16 = 'SOSWEET87 = 54*33 #LIVITOTO';
+var namagroup16 = 'PRIA4D';
+var Comment16 = '#PRIA4D ( SOSWEET87 ) : 54*33*13';
 
 var namagroup17 = 'LING';
 var Comment17 = 'LING77 (SHINY111) 07*65';
