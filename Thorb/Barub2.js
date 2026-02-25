0fee9da5dda8aeecc7b873f06241939331d69fc0
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-02-25 22:01:03 +0700

    Update Barub2.js

--- Thorb/Barub2.js
+++ Thorb/Barub2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru B2
 // @namespace    http://tampermonkey.net/
-// @version      4.1
+// @version      4.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub2.js
@@ -62,8 +62,8 @@ var Comment14 = '#BLITAR4D (QOPILU09) = 44*03';
 var namagroup15 = 'UPINSLOT';
 var Comment15 = '#UPINSLOT ( QONCCER88 ) 59*50*30 BETTING';
 
-var namagroup16 = 'PRIA4D';
-var Comment16 = '#PRIA4D ( QUANTUM73 ) : 31*53*22';
+var namagroup16 = 'BERSAMA';
+var Comment16 = '#BERLIANJITU#QUANTUM73*31*53*';
 
 var namagroup17 = 'LING';
 var Comment17 = 'LING77 (QONCCER88) 67*49';
@@ -76,7 +76,7 @@ var Comment18 = 'Grup BEBEK 22';
 var SCRIPT_NAME = Comment18
 var refresh = 40;
 var URLADMIN = "https://raw.githubusercontent.com/natasyabimosakti/ADMIN/main/adminer2.json"
-var keyword = ["ROOM","RUM","𝑹𝑶𝑶𝑴","𝗥𝟬𝟬𝗠","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌"]
+var keyword = ["ROOM","RUM","🆁🅾🅾🅼","𝑹𝑶𝑶𝑴","𝗥𝟬𝟬𝗠","𝗥𝗢𝗢𝗠","LOMBA","𝗟𝗢𝗠𝗕𝗔","𝐋𝐎𝐌𝐁𝐀","LIMBA","ROM","R00M","login","𝐑𝐎𝐎𝐌"]
 var Backlist =["pemenang lomba","rekap","natidulu","room lomba freebet","prediksi","result","juara lomba","r3k4p","TTM","r3kap","rek4p"]
 var isCommenting = false;
 var EXPIRATION_MS = 8 * 60 * 1000; // 5 minutes
