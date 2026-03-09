18afa62f315617c0862e5abf0c32422b6924f3b8
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-03-09 20:37:48 +0700

    Update lamab2.js

--- grupb/lamab2.js
+++ grupb/lamab2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA B2
 // @namespace    http://tampermonkey.net/
-// @version      3.43
+// @version      3.44
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab2.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab2.js
@@ -41,7 +41,7 @@ var Comment7 = 'ASEPTOGEL QRISS08 84*97*51';
 var namagroup8 = 'KIKOTOTO';
 var Comment8 = '#KIKOTOTO (QIBAAT50) = 58*96';
 
-var namagroup9 = 'EMPAT';
+var namagroup9 = 'RUMUSAN';
 var Comment9 = '#HATORIBET*QUANTUM73*00*01*56';
 
 var namagroup10 = 'JNETOTO';
