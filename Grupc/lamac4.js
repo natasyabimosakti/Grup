fa46daa0ab1fa9e7f20a958958258aa23230b092
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-03-11 11:29:11 +0700

    Update lamac4.js

--- Grupc/lamac4.js
+++ Grupc/lamac4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA CACING 14
 // @namespace    http://tampermonkey.net/
-// @version      4.6
+// @version      4.7
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupc/lamac4.js
@@ -61,7 +61,7 @@ var Comment13 = 'Grup CACING 14';
 var namagroup14 = 'XXXXGTO';
 var Comment14 = 'Grup CACING 14';
 
-var namagroup15 = 'RUMUSAN';
+var namagroup15 = 'INFO';
 var Comment15 = '#HATORIBET*WAWAN92*08*63*54';
 
 var namagroup16 = 'KIKOTOTO';
