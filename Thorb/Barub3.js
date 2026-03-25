45d2e1d8582f2dc9c91cc0917dbf8c2eb30212f3
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-03-25 11:35:31 +0700

    Update Barub3.js

--- Thorb/Barub3.js
+++ Thorb/Barub3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru B3
 // @namespace    http://tampermonkey.net/
-// @version      4.1
+// @version      4.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorb/Barub3.js
@@ -62,8 +62,8 @@ var Comment14 = '#BLITAR4D (ROTTAN131) = 28*52';
 var namagroup15 = 'UPINSLOT';
 var Comment15 = '#UPINSLOT ( ROLOKAB111 ) 71*95*06 BETTING';
 
-var namagroup16 = 'BERSAMA';
-var Comment16 = '#BERLIANJITU#RISE989*42*91*';
+var namagroup16 = 'BERAS889';
+var Comment16 = '#BERAS889 (QRIBO3) : 42*91*29';
 
 var namagroup17 = 'LING';
 var Comment17 = 'LING77 (ROLOKAB111) 66*95';
