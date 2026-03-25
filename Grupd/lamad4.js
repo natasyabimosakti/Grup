7e030061f1d361be9909d9732c625a2dc3a71955
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-03-25 11:41:32 +0700

    Update lamad4.js

--- Grupd/lamad4.js
+++ Grupd/lamad4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA DEMAM 14
 // @namespace    http://tampermonkey.net/
-// @version      5.4
+// @version      5.5
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupd/lamad4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupd/lamad4.js
@@ -53,8 +53,9 @@ var Comment11 = 'SAHARA888 ( GEMBOS20 ) 17*08*78';
 var namagroup12 = 'DAGELAN';
 var Comment12 = 'DAGELAN4D(GION201) : 68*03*46';
 
-var namagroup13 = 'BERSAMA';
-var Comment13 = '#BERLIANJITU#GENDON70*97*82*';
+var namagroup13 = 'BERAS889';
+var Comment13 = '#BERAS889 (GENDON70) : 97*82*18';
+
 
 //BATAS BO 1
 
