e86ce9329d4da066918ef6f4b8127664c9c29e40
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-04-03 10:59:57 +0700

    Update lamaa3.js

--- Grupa/lamaa3.js
+++ Grupa/lamaa3.js
@@ -48,7 +48,7 @@ var namagroup10 = 'NEMO4D';
 var Comment10 = 'NEMO4D (MADSOLAR77) : 04*49*21';
  
 var namagroup11 = 'ALTERT0T0';
-var Comment11 = '#ALTERTOTO#MALIAN19*21*60*80';
+var Comment11 = '#ALTERTOTO#FANTA15*29*55*17';
  
 var namagroup12 = 'DAGELAN';
 var Comment12 = 'DAGELAN4D(MASIKA12) : 77*38*94';
