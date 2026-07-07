9a59218848410a4760128cda40189eb34471ff81
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-07-07 22:31:37 +0700

    Update lamaa1.js

--- Grupa/lamaa1.js
+++ Grupa/lamaa1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA ANGIN 11
 // @namespace    http://tampermonkey.net/
-// @version      5.7
+// @version      5.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Grupa/lamaa1.js
