24fd6c3bf1e658d8e4c6ffe4a87efa9a80538b61
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-07-08 09:54:21 +0700

    Update lamab4.js

--- grupb/lamab4.js
+++ grupb/lamab4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI LAMA B4
 // @namespace    http://tampermonkey.net/
-// @version      3.48
+// @version      3.49
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab4.js
