aef500e96e18e4e58284315f156e71a2fd0aa7a7
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-07-08 09:53:42 +0700

    Update lamab1.js

--- grupb/lamab1.js
+++ grupb/lamab1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ORI Lama B1
 // @namespace    http://tampermonkey.net/
-// @version      5.3
+// @version      5.4
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/grupb/lamab1.js
