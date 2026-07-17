b197b0817f07ea1929e734c3b4d619085faad6a1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-07-17 10:21:18 +0700

    Update Barua3.js

--- Thora/Barua3.js
+++ Thora/Barua3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru ANGIN 23
 // @namespace    http://tampermonkey.net/
-// @version      5.1
+// @version      5.2
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua3.js
@@ -21,7 +21,7 @@ var namagroup18 = 'Jawatengah';
 var Comment18 = 'Angin23';
 
 
-   // --- 1. ANTI-THROTTLE & KEEP-ALIVE (Solusi Tab Background) ---
+ // --- 1. ANTI-THROTTLE & KEEP-ALIVE (Solusi Tab Background) ---
     (function () {
         // Memaksa properti visibility agar selalu 'visible'
         Object.defineProperty(document, 'visibilityState', { value: 'visible', writable: true });
