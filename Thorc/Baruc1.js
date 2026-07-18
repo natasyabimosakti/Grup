fc9ccd080c0b73752857eea2fe8db4f2b7c70a9e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-07-18 22:41:37 +0700

    Update Baruc1.js

--- Thorc/Baruc1.js
+++ Thorc/Baruc1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BAru CACING 21
 // @namespace    http://tampermonkey.net/
-// @version      4.9
+// @version      5.0
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorc/Baruc1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thorc/Baruc1.js
