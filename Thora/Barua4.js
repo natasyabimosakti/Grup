ac84c6dd20b298b594e8a32ee3515c76fc641e36
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2026-09-06 21:32:25 +0700

    Update Barua4.js

--- Thora/Barua4.js
+++ Thora/Barua4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Baru ANGIN 24
 // @namespace    http://tampermonkey.net/
-// @version      4.7
+// @version      4.8
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Grup/refs/heads/main/Thora/Barua4.js
